5ae5fd67f6629bbd6f45f07b93a405c8ed460bf9
ag-shubham <shubhamsatnalika64@gmail.com>
2022-10-04 14:12:28 +0530

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+const mongoURI = process.env.DB;
+
+mongoose
+    .connect(mongoURI)
+    .then(() => console.log("Connected to Database"))
+    .catch((err) => console.log(err));
+
+app.use(cors()) ;
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    next();
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
