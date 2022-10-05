79ded68c3c4a8ecc6ce722f945a046a32f481e48
ag-shubham <shubhamsatnalika64@gmail.com>
2022-10-05 13:11:42 +0530

    read-update-del operations

--- server/routes/events.js
+++ server/routes/events.js
@@ -18,4 +18,67 @@ router.post("/createEvent",
         }
 });
 
+//Route for updating an event
+router.get("/fetchEvents",
+    async(req,res) => {
+        try{
+            const allEvents = await Event.find({});
+            res.json(allEvents);
+        }
+        catch(err){
+            console.error(err.message);
+            res.status(500).send("Internal Server Error");
+        }
+    }
+);
+
+//Route for updating an event
+router.put("/updateEvent/:id",
+    async(req,res) =>{
+        try{
+            const{title,content} = req.body ;
+            
+            //creating a new object for storing values to be changed
+            const newEvent = {} ;
+            if(title) newEvent.title = title ;
+            if(content) newEvent.content = content ;
+
+            //Checking whether event to be updated exists in database or not
+            const event = await Event.findById(req.params.id);
+            if(!event) {
+                return res.status(404).json("Not Found");
+            }
+
+            //Updation
+            const UpdatedEvent = await Event.findByIdAndUpdate(req.params.id, {$set:newEvent}, {new:true});
+            res.json({UpdatedEvent});
+        }
+        catch(err){
+            console.error(err.message);
+            res.status(500).send("Internal Server Error") ;
+        }
+    }
+);
+
+//Route for deleting an event
+router.delete("/deleteEvent/:id",
+    async(req,res) => {
+        try{
+            //Checking whether event to be updated exists in database or not
+            const event = await Event.findById(req.params.id);
+            if(!event) {
+                return res.status(404).json("Not Found");
+            }
+
+            //Deletion
+            const deletedEvent = await Event.findByIdAndDelete(req.params.id) ;
+            res.json({deletedEvent}) ;
+        }
+        catch(err) {
+            console.error(err.message);
+            res.status(500).send("Internal Server Error");
+        }
+    }
+);
+
 module.exports = router;
